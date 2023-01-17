eb2723c6f8d6e54f6ca1576596516c011415e537
kaibad <badukailash001@gmail.com>
2023-01-17 23:31:58 +0545

    first try)

---
+++ print()/exercise2.py
@@ -0,0 +1,8 @@
+# String Manipulation exercise
+# String Concatenationis done with"+" sign
+# e.g print("hello"+"kailash")
+# New line can be created with a backlash and n
+print("String " + "Manipulation " + "exercise")
+print('String ' + ' Concatenation ' + ' done with ' + '"+" ' + 'sign')
+print('e.g ' + 'print("hello"+"kailash")')
+print("New" +" "+ "Line " + "can " + "be " + "created " + "with " + "a " + "backlash " + "and " + "n")
---
+++ print()/string_manipulation.py
@@ -0,0 +1,8 @@
+print("hello..\nhello world..")
+print("hello"+"kailash")
+print("hello"+" kailash")
+print("hello "+"kailash")
+print("hello"+" "+"kailash")
+# --------INDENTATION-ERROR-------
+#  print("hello"+" "+"kailash")
+# START WRITING THE CODE IN THE BEGINING OF THE LINE
